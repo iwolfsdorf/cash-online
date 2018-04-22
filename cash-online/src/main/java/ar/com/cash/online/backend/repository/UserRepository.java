d80ce9a2caf2d0d76bd7dca6514dddcd4b72fb5f
iwolfsdorf <wolfsdorf.ivan@gmail.com>
2018-04-22 20:31:50 -0300

    Se agrega annotations Repository

--- cash-online/src/main/java/ar/com/cash/online/backend/repository/UserRepository.java
+++ cash-online/src/main/java/ar/com/cash/online/backend/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package ar.com.cash.online.backend.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import ar.com.cash.online.backend.model.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
 }
